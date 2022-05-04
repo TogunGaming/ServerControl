9e0aba771457ace3e7901be1955d109b3fda0ef6
TogunGaming <76750598+TogunGaming@users.noreply.github.com>
2022-05-04 22:48:49 +0200

    Update controlCommand.java

--- src/main/java/eu/togun/commands/controlCommand.java
+++ src/main/java/eu/togun/commands/controlCommand.java
@@ -1,12 +1,13 @@
 package eu.togun.commands;
 
+// Message Sender Dependencies
 import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.CommandSender;
 import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.plugin.Command;
 
-//
+// Http Request Libraries
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.http.HttpRequest;
@@ -23,12 +24,18 @@ public class controlCommand extends Command {
     public void execute(CommandSender commandSender, String[] args) {
         try {
             HttpRequest request = HttpRequest.newBuilder()
-                    .uri(new URI("https://admin.catnetwork.eu/api/"))
+                    .uri(new URI("https://admin.catnetwork.eu/api/servers"))
                     .timeout(Duration.of(10, SECONDS))
                     .GET()
                     .build();
-        } catch (URISyntaxException e) {
-            e.printStackTrace();
+        } catch (URISyntaxException err) {
+            err.printStackTrace();
+        }
+        
+        if (commandSender instanceof ProxiedPlayer) {
+            ProxiedPlayer player = (ProxiedPlayer) commandSender;
+
+            player.sendMessage(new TextComponent(ChatColor.GRAY + "Server List: " + ChatColor.GREEN + ping));
         }
     }
 }
